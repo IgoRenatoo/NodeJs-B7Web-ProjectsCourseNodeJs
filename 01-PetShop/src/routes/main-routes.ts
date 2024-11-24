a066b7c46ff0c9360f6a3f806eccbcc075e7226b
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-11-24 15:01:40 -0300

    chore: :wrench: Modified main routes sctructure

--- 01-PetShop/src/routes/main-routes.ts
+++ 01-PetShop/src/routes/main-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import * as PageControllers from "../controllers/page-controllers";
+import { home, dog, cat, fish} from "../controllers/page-controllers";
 
 export const router = Router();
 
-router.get("/", PageControllers.home);
-router.get("/dog", PageControllers.dog);
-router.get("/cat", PageControllers.cat);
-router.get("/fish", PageControllers.fish);
+router.get("/", home);
+router.get("/dog", dog);
+router.get("/cat", cat);
+router.get("/fish", fish);

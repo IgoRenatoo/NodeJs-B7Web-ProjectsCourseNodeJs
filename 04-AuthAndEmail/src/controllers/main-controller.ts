d050b90b09d83251c3bea2234ab7f6954f771bdb
IgoRenatoo <igor.renato.oliveira@gmail.com>
2024-12-10 21:16:31 -0300

    feat: :sparkles: Added middleware Basic authentication

--- 04-AuthAndEmail/src/controllers/main-controller.ts
+++ 04-AuthAndEmail/src/controllers/main-controller.ts
@@ -1,31 +1,33 @@
-import { dbPrisma } from './../database/prisma-database'
 import { Request, Response } from 'express'
+import { addUser, listUsers } from '../services/main-service'
 
-export const addUser = async (req: Request, res: Response): Promise<Response> => {
+export const createUser = async (req: Request, res: Response): Promise<void> => {
   const { name, email, age } = req.body
 
-  if (!name || !email) res.status(404).json({ error: 'name and email are required.' })
-
-  const existingUser = await dbPrisma.userAuth.findFirst({ where: { OR: [{ name }, { email }] } })
-  if (existingUser) {
-    return res.status(400).json({
-      error: 'User with this name or email already exists. Your password is "motocorno"'
-    })
+  if (!name || !email) {
+    res.status(400).json({ error: 'name and email are required.' })
+    return
   }
 
-  try {
-    const newUser = await dbPrisma.userAuth.create({
-      data: {
-        name: name,
-        email: email,
-        age: age
-      }
-    })
+  const newUser = await addUser(name, email, age)
 
-    return res.status(201).json({ newUser })
-  } catch (error) {
-    console.error('Error creating user:', error)
+  if (newUser.success) {
+    res.status(201).json({ Request: newUser })
+    return
+  } else {
+    res.status(400).json({ Request: newUser.message })
+    return
+  }
+}
 
-    return res.status(500).json({ error: 'An error occurred while creating the user.' })
+export const listAllUsers = async (req: Request, res: Response): Promise<void> => {
+  const users = await listUsers()
+
+  if (users.sucess) {
+    res.status(200).json({ Request: users })
+    return
+  } else {
+    res.status(400).json(users.message)
+    return
   }
 }
